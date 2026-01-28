40145e1ff9ea5559d4c76ba9b15c6e39f5c7756c
Carlos Jimenez <cjimenezber@gmail.com>
2026-01-27 17:03:04 -0700

    Add modal component and success notice.

---
+++ ComponentTagHelpers/BootstrapModalComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using TechGems.RazorComponentTagHelpers;
+
+namespace PacsTest.ComponentTagHelpers;
+
+
+[HtmlTargetElement("bs-modal")]
+public class BootstrapModalComponent : RazorComponentTagHelper
+{
+
+    public BootstrapModalComponent() : base("~/Views/Components/BootstrapModal.cshtml")
+    {
+
+    }
+}
